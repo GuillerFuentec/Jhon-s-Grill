3c0f8587aa009842dbf0ae5e531494ca8b2f3a2c
GuillerFuentec <copelloguillermo60@gmail.com>
2025-09-20 21:33:58 -0400

    pre-pull

---
+++ web/config.js
@@ -0,0 +1,8 @@
+﻿// config.js
+export const config = {
+  API_URL: "http://localhost:1337/api",
+  DOMINIO: "tudominio.com",
+  STRIPE_PUBLISHABLE_KEY:"pk_test_51S7jjR0f3vhjJRNrZPHxcQYePOmKTVx1y8qpZJ9bKTySvANUdvs4BBWfxkrCgUz2WzQi39RhEad4UteI1TRCkDcf00nejnTkjB",
+  BARBERSHOP_ADDRESS:"Av. Winston Churchill",
+  MODE: "development",
+};
